d645351c12ee9fc7821cd9aa6b419dd3112380e2
ahmadsyaf27 <ahmadspro99@gmail.com>
2025-11-12 23:50:49 +0700

    partner modal up

--- models/partner/partner.go
+++ models/partner/partner.go
@@ -3,49 +3,77 @@ package partner
 import (
 	"time"
 
-	partner_owner "mobile-directory-bussines/models/partner_owner"
+	"mobile-directory-bussines/models/master/category"
+	"mobile-directory-bussines/models/master/subcategory"
+	"mobile-directory-bussines/models/master/secondsubcategory"
+	"mobile-directory-bussines/models/master/region"
+	"mobile-directory-bussines/models/partnerowner"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type Partner struct {
-	ID                  string     `json:"id" gorm:"type:char(36);primaryKey"`
-	PartnerOwnerID      *uint64    `json:"partner_owner_id" gorm:"not null"`
-	SalesID             *uint64    `json:"sales_id"`
-	BestGroupID         *uint64    `json:"best_group_id"`
-	ProductPacketID     *uint64    `json:"product_packet_id"`
-	RangeMonth          int        `json:"range_month" gorm:"default:1"`
-	QtyMonth            int        `json:"qty_month" gorm:"default:1"`
-	Code                string     `json:"code" gorm:"type:varchar(50);unique;not null"`
-	Name                string     `json:"name" gorm:"type:varchar(100);not null;index"`
-	Email               *string    `json:"email" gorm:"type:varchar(255);unique"`
-	MobileNo            string     `json:"mobile_no" gorm:"type:varchar(15);not null"`
-	WorkingHourStart    *time.Time `json:"working_hour_start"`
-	WorkingHourEnd      *time.Time `json:"working_hour_end"`
-	StatusTxt           string     `json:"status_txt" gorm:"type:varchar(20);default:'active'"`
-	Description         *string    `json:"description" gorm:"type:text"`
-	Address             *string    `json:"address" gorm:"type:text"`
-	Rt                  *string    `json:"rt" gorm:"type:varchar(5)"`
-	Rw                  *string    `json:"rw" gorm:"type:varchar(5)"`
-	Latitude            float64    `json:"latitude" gorm:"not null"`
-	Longitude           float64    `json:"longitude" gorm:"not null"`
-	ProvinceCode        *string    `json:"province_code" gorm:"type:varchar(10)"`
-	CityCode            *string    `json:"city_code" gorm:"type:varchar(10)"`
-	DistrictCode        *string    `json:"district_code" gorm:"type:varchar(10)"`
-	VillageCode         *string    `json:"village_code" gorm:"type:varchar(10)"`
-	PaymentMethod       string     `json:"payment_method" gorm:"type:enum('manual','payment_gateway','not_set');default:'not_set'"`
-	PaymentStatus       *string    `json:"payment_status" gorm:"type:varchar(255)"`
-	ManualPaymentStatus string     `json:"manual_payment_status" gorm:"type:varchar(100);default:'pending'"`
-	UrlWebsite          *string    `json:"url_website" gorm:"type:varchar(255)"`
-	FbSosmed            *string    `json:"fb_sosmed" gorm:"type:varchar(155)"`
-	IgSosmed            *string    `json:"ig_sosmed" gorm:"type:varchar(155)"`
-	TiktokSosmed        *string    `json:"tiktok_sosmed" gorm:"type:varchar(155)"`
-	PhoneNo             *string    `json:"phone_no" gorm:"type:varchar(50)"`
-	SubDomainRequest    *string    `json:"sub_domain_request" gorm:"type:varchar(255)"`
-	CreatedAt           *time.Time `json:"created_at"`
-	UpdatedAt           *time.Time `json:"updated_at"`
+	ID                       string         `gorm:"type:char(36);primaryKey" json:"id"`
+	PartnerOwnerID           string         `gorm:"type:char(36)" json:"partner_owner_id"`
+	SellerType               string         `json:"seller_type"` // general_partner / prefered_partner
+	SalesID                  string         `json:"sales_id"`
+	BestGroupID              string         `json:"best_group_id"`
+	VerificatorID            string         `json:"verificator_id"`
+	RangeMonth               string         `json:"range_month"`
+	QtyMonth                 int            `json:"qty_month"`
+	Code                     string         `json:"code"`
+	Name                     string         `json:"name"`
+	Email                    string         `json:"email"`
+	MobileNo                 string         `json:"mobile_no"`
+	WorkingHourStart         string         `json:"working_hour_start"`
+	WorkingHourStop          string         `json:"working_hour_stop"`
+	Address                  string         `json:"address"`
+	Rt                       string         `json:"rt"`
+	Rw                       string         `json:"rw"`
+	Latitude                 string         `json:"latitude"`
+	Longitude                string         `json:"longitude"`
+	ProvinceCode             string         `json:"province_code"`
+	CityCode                 string         `json:"city_code"`
+	DistrictCode             string         `json:"district_code"`
+	VillageCode              string         `json:"village_code"`
+	PaymentMethod            string         `json:"payment_method"`
+	PaymentStatus            string         `json:"payment_status"`
+	ManualPaymentStatus      string         `json:"manual_payment_status"`
+	UrlWebsite               string         `json:"url_website"`
+	SubDomainRequest         string         `json:"sub_domain_request"`
+	StatusTxt                string         `json:"status_txt"`
+	VerificationSellerStatus string         `json:"verification_seller_status_txt"`
+	VerificationSellerAt     *time.Time     `json:"verification_seller_at"`
+	StatusStreamTxt          string         `json:"status_stream_txt"`
+	FbSosmed                 string         `json:"fb_sosmed"`
+	IgSosmed                 string         `json:"ig_sosmed"`
+	TiktokSosmed             string         `json:"tiktok_sosmed"`
+	PhoneNo                  string         `json:"phone_no"`
+	StoreDescription         string         `json:"store_description"`
+	BusinessType             string         `json:"bussines_type"`
+	NIK                      string         `json:"nik"`
+	NPWP                     string         `json:"npwp"`
+	CreatedAt                time.Time      `json:"created_at"`
+	UpdatedAt                time.Time      `json:"updated_at"`
+	DeletedAt                gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
 
-	PartnerOwner *partner_owner.PartnerOwner `json:"partner_owner,omitempty" gorm:"foreignKey:PartnerOwnerID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	// RELATIONS
+	PartnerOwner *partnerowner.PartnerOwner `gorm:"foreignKey:PartnerOwnerID" json:"partner_owner,omitempty"`
+	Province     *region.Province     `gorm:"foreignKey:ProvinceCode;references:Code" json:"province,omitempty"`
+	City         *region.City         `gorm:"foreignKey:CityCode;references:Code" json:"city,omitempty"`
+	District     *region.District     `gorm:"foreignKey:DistrictCode;references:Code" json:"district,omitempty"`
+	Village      *region.Village      `gorm:"foreignKey:VillageCode;references:Code" json:"village,omitempty"`
+
+	Categories          []*category.Category          `gorm:"many2many:partners_categories;foreignKey:ID;joinForeignKey:PartnerID;References:ID;joinReferences:CategoryID" json:"categories,omitempty"`
+	SubCategories       []*subcategory.SubCategory       `gorm:"many2many:partners_sub_categories;foreignKey:ID;joinForeignKey:PartnerID;References:ID;joinReferences:SubCategoryID" json:"sub_categories,omitempty"`
+	SecondSubCategories []*secondsubcategory.SecondSubCategory `gorm:"many2many:partners_second_sub_categories;foreignKey:ID;joinForeignKey:PartnerID;References:ID;joinReferences:SecondSubCategoryID" json:"second_sub_categories,omitempty"`
 }
 
-func (Partner) TableName() string {
-	return "partners"
+// Auto generate UUID before create
+func (p *Partner) BeforeCreate(tx *gorm.DB) (err error) {
+	if p.ID == "" {
+		p.ID = uuid.New().String()
+	}
+	return
 }
